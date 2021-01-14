f82dc4a9d64e7a8882918c9a974cd777e44efa9b
Favour Toghanro <favour.toghanro@stu.pfisd.net>
2021-01-14 23:28:12 +0000

    added actual qualifer

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Elite')
+from textblob import Textblob
+
+file=open ("1.txt,r+")
+a= file 1.read()
+
+print("original text : ",+str(a))
+
+b=Textblob(a)
+
+print("corrected text : "+str(b.correct()) )
+file1.close()
+
+d=open ("1.txt","w")
+d.write(str(b.correct()))
+d.close()
